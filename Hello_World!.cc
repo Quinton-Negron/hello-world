5196d87fa00293eb9f8008777e496e509f66a502
Quinton-Negron <44473566+Quinton-Negron@users.noreply.github.com>
2019-09-10 21:29:14 -0400

    Add files via upload

---
+++ Hello_World!.cc
@@ -0,0 +1,12 @@
+//Name:Quinton Negron
+//Date:December 3, 2018
+//Lab 53: Hello World in C++
+
+#include <iostream>        //The built-in library for input/output
+using namespace std;       //The names of standard definitions
+
+int main()                 //C++ programs all have a main() function
+{
+  cout << "Hello, World!"; //Print "Hello, World!" to the terminal
+  return 0;                //Standard way to end function
+}
